8288918e6de03b5ba6a138c54bb3ffd8c5061ebe
Coffee-Expert <abhishekevingomesofficial@gmail.com>
2024-09-21 16:30:05 +0530

    f

--- networking_app/plugs/urls.py
+++ networking_app/plugs/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PlugRequestViewSet, MessageViewSet
+from .views import PlugRequestViewSet, MessageViewSet, Root
 
 router = DefaultRouter()
 router.register(r'plug-requests', PlugRequestViewSet)
 router.register(r'messages', MessageViewSet)
+router.register(r'', Root)
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- networking_app/plugs/views.py
+++ networking_app/plugs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import PlugRequest, Message
 from .serializers import PlugRequestSerializer, MessageSerializer
 
@@ -12,3 +13,8 @@ class PlugRequestViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()   
     serializer_class = MessageSerializer  
+
+class Root():
+    def get(self, request):
+        return Response({'message': 'Server is running'})
+    
